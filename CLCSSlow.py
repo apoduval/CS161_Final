60ad4e5b43d43ef51c4e304b545f27b821162138
elanh <halpernesta@DN0a23230d.SUNet>
2018-03-10 17:23:24 -0800

    Implemented the cut(A,k) method

--- CLCSSlow.py
+++ CLCSSlow.py
@@ -5,15 +5,13 @@ import numpy as np
 arr = np.zeros((2048, 2048), dtype=int)
 
 def cut(A,k):
-	n  = A.length()
-	if k < 0 || k >= n:
-		k = k%n
-
-	return substring(A,k,n) + substring(A,0,k);
+	n  = len(A)
+	if k < 0 | k >= n:
+		k = k % n
+	return  A[k:n] + A[0:k] #substring(A,k,n) + substring(A,0,k);
 
 # Only cut over one of the strings while keeping the other one fixed
 # do not zero/reset the DP array between calls to the LCS method 
-# use Python string/list slicing for the cutting.
 def CLCSSlow():
 	for i in range(0, m): # should i start from 0 or 1?
 		for j in range(0, n):
@@ -37,9 +35,7 @@ def main():
 	if len(sys.argv) != 1:
 		sys.exit('Usage: `python LCS.py < input`')
 	
-	for l in sys.stdin:
-		A,B = l.split()
-		print LCS(A,B)
+	print(cut("abcd",0))
 	return
 
 if __name__ == '__main__':
