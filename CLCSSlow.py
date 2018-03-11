67b0a3659aa5cdd22d08533777cfdeb4ec709bb1
elanh <halpernesta@DN0a23230d.SUNet>
2018-03-10 17:11:29 -0800

    Added pseudocode to CLCSSlow and CLCSFast

--- CLCSSlow.py
+++ CLCSSlow.py
@@ -1,25 +1,37 @@
 #CLCSSlow Implementation
+import sys
+import numpy as np
 
-# Compute shortest path of mid (pm) using dynamic programming bounded by pl and pu
-def SingleShortestPath(A,B,m,pl,pu)
+arr = np.zeros((2048, 2048), dtype=int)
+
+def cut(A,k):
+	n  = A.length()
+	if k < 0 || k >= n:
+		k = k%n
+
+	return substring(A,k,n) + substring(A,0,k);
+
+# Only cut over one of the strings while keeping the other one fixed
+# do not zero/reset the DP array between calls to the LCS method 
+# use Python string/list slicing for the cutting.
+def CLCSSlow():
+	for i in range(0, m): # should i start from 0 or 1?
+		for j in range(0, n):
+		 	LCS(cut(A,i), cut(B,j))
 
 
-def FindShortestPath(A,B,p,l,u):
-     if (u-l <= 1) return
-     mid = (l+u)/2
-     p[mid] = SingleShortestPath(A,B,mid,p[l],p[u])
-     FindShortestPath(A,B,p,l,mid)
-     FindShortestPath(A,B,p,mid,u)
-     
-
-
-def CLCSSlow(A,B):
-
+def LCS(A,B):
 	m = len(A)
 	n = len(B)
-    p = []# array of arrays to represent paths?
-    FindShortestPath(A,B,p,0,m)
 
+	for i in range(1,m+1):
+		for j in range(1,n+1):
+			if A[i-1] == B[j-1]:
+				arr[i][j] = arr[i-1][j-1]+1
+			else:
+				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+
+	return arr[m][n]
 
 def main():
 	if len(sys.argv) != 1:
