19182aec7f940d5f212dd5acd70f8d2c4bcd1517
elanh <halpernesta@DN0a1fcc57.SUNet>
2018-03-12 12:51:26 -0700

    Added code to Fast CLCS

--- CLCSFast.py
+++ CLCSFast.py
@@ -1,63 +1,78 @@
 #CLCSFast 
+import sys
+import numpy as np
+
+arr = np.zeros((2048,2048), dtype=int)
+p = [[]]
 
 # Returns the length of the LCS
-def LCS(A,B):
-	m = len(A)
-	n = len(B)
+# def LCS(A,B):
+# 	m = len(A)
+# 	n = len(B)
 
-	for i in range(1,m+1):
-		for j in range(1,n+1):
-			if A[i-1] == B[j-1]:
-				arr[i][j] = arr[i-1][j-1]+1
-			else:
-				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
-	return arr[m][n]
+# 	for i in range(1,m+1):
+# 		for j in range(1,n+1):
+# 			if A[i-1] == B[j-1]:
+# 				arr[i][j] = arr[i-1][j-1]+1
+# 			else:
+# 				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+# 	return arr[m][n]
 
 # Compute shortest path of mid (pm) using dynamic programming bounded by pl and pu
+# find most optimal path from (mid,0) to (mid+m, n)
+# Store an array of paths then choose the shortest one
+# Every time you look at a node check if it is in the bounds 
+# Basically LCS but when we hit a bound use the bound path 
+# arr[i][j] returns the length of the LCS 
 def SingleShortestPath(A,B,mid,pl,pu):
-	 # find most optimal path from (mid,0) to (mid+m, n)
-	 # Store an array of paths then choose the shortest one
-	 # Every time you look at a node check if it is in the bounds 
-	 # Basically LCS but when we hit a bound use the bound path 
-
 	m = len(A)
 	n = len(B)
-
-	for i in range(1,m+1):
-		for j in range(1,n+1):
-			if A[i-1] == B[j-1]:
-				arr[i][j] = arr[i-1][j-1]+1
-			else:
-				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
-	return arr[m][n]
+	print "pl is",  pl
+	print "pu is", pu
+	path = [0 for i in range(m)]
+	for i in range(m): # keep the same should it be mid to mid+m?
+		index =  mid+i -m if mid + i >= m else mid + i
+		l = pl[index]
+		u = pu[index]
+		print "char", A[index]
+		for j in range(1,n+1):      #(1,n+1):
+			if (j <= u) & (j >= l):
+				if A[index-1] == B[j-1]:
+					arr[index][j] = arr[index-1][j-1]+1
+				else:
+					arr[index][j] = max(arr[index-1][j], arr[index][j-1])
+					if arr[index][j] == arr[index][j-1]:
+						path[index] = j-1
+				path[index] = j
+	print "path is", path
+	return path		
+	#return arr[mid+m][n] #NEED TO RETURN AN ARRAY NOT A NUMBER 
 
 
 
 def FindShortestPath(A,B,p,l,u):
-     if (u-l <= 1) return
-     mid = (l+u)/2
-     p[mid] = SingleShortestPath(A,B,mid,p[l],p[u])
-     FindShortestPath(A,B,p,l,mid)
-     FindShortestPath(A,B,p,mid,u)
-     
+	 if (u-l <= 1):
+		return
+	 mid = (l+u)/2
+	 print "mid is", mid, "l is", l, "u is", u
+	 p[mid] = SingleShortestPath(A,B,mid,p[l],p[u])
+	 FindShortestPath(A,B,p,l,mid)
+	 FindShortestPath(A,B,p,mid,u)
+	 
 
 
 def CLCSFast(A,B):
 	m = len(A)
 	n = len(B)
-    p = [][]# array of arrays to represent paths?
-    FindShortestPath(A,B,p,0,m)
+	p = [[0 for i in range(m+1)] for j in range(m+1)] # needs to be a gloabl variable
+	FindShortestPath(A,B,p,0,m)
+	# Find the shortest pi from p
+	min = p[0]
+
 
 
 
 def main():
-	if len(sys.argv) != 1:
-		sys.exit('Usage: `python LCS.py < input`')
-	
-	for l in sys.stdin:
-		A,B = l.split()
-		print LCS(A,B)
-	return
-
+	print CLCSFast("ABCDE", "ABABB")
 if __name__ == '__main__':
 	main()
