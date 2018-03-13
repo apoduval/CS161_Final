cb7b401ebb74c38bf1bdceaa7a839e46a92b233a
Olivia Gregory <oliviaeg@stanford.edu>
2018-03-13 11:30:50 -0700

    Added backtracing pseudo code and OH notes, and also played with bounds a little bit

--- CLCSFast.py
+++ CLCSFast.py
@@ -5,7 +5,7 @@ import numpy as np
 #initialize dp array to be 500x 500 because that is max string length
 #initialize path to empty 2d array (will be initialized by size later)
 #IMPORTANT NOTE: NEED A WAY TO CLEAR RELEVANT LENGTHS BETWEEN DIFFERENT STRING INPUTS
-arr = np.zeros((20,10), dtype=int)
+arr = np.zeros((10,10), dtype=int)
 
 #path is 2 concatenated arrays to represent upper and lower bounds
 p = [[[]],[[]]]
@@ -22,22 +22,62 @@ LCS = []
 
 #path holds index of B string associated with each possible m value
 
+
+#!!!!!WHAT WAS SAID DURING OFFICE HOURS!!!!!
+#First check if you can take the diagonal! The only way you can take the diagonal
+# is if the diagonal number is one less than your current number AND the chars are the same.
+#If these both check out, take the diagonal.
+
+#If not, check the top and left.
+#If one is the same number as your cur, but the other isn't,
+#you have to go in that direction.
+
+#If both left and top are the same as your cur,
+#you can take either!
+
+#Make sure you never cross the lines--
+# i. e., check your upper and lower bounds, and your horizontal bounds (m to mid+m).
+
 def PathBacktrace(mid,m,l,u):
+	#note: need to pass in arr
+	#for (int i = m; i > 0; i++) :
+	#  cur = arr[i][j]
+	#  diag = arr[i - 1][j - 1]
+	#  if (diag == cur - 1) && A[diag] == B[cur]:
+	#	#if the diagonal is one less and the char is the same
+	#    path += diag
+	#	 i - 1; j - 1;
+	#  else:
+	#    left = arr[i - 1][j]
+	#    top = arr[i][j-1]
+	#      if (left == top == cur):
+	#         #can go left or top
+	#         path += left;
+	#		  i - 1;
+	#	   if (left = cur):
+	#		path += left;
+	#		i - 1;
+	#		if (top = cur):
+	#		path += top;
+	#		j - 1;
+
+	#first lower, first upper
 	if mid == 1:
 		 p[0][1] = [0, 1, 2, 3, 3]
 		 p[1][1] = [0, 1, 2, 2, 3]
+	#second lower, second upper
 	elif mid == 2:
 	 	p[0][2] = [0, 3, 4, 4, 4]
 	 	p[1][2] = [0, 2, 4, 4, 4]
-	else: 
+	#third lower and third upper
+	else:
 	    p[0][3] =  p[1][3]  = [0, 3, 4, 5, 5]
-	
+
 
 def SingleShortestPath(A,B,mid, l, u):
 
 	global p
 	global arr
-
 
 	m = len(A)/2
 	print "m",  m
@@ -47,34 +87,40 @@ def SingleShortestPath(A,B,mid, l, u):
 	print p
 
 	#set upper and lower path bounds
-	noPath = (l == 0) 
+	noPath = (l == 0)
 
-	#1 : lower, 2: upper
+	#0 : lower, 1: upper
 	pl0 = p[0][l]
 	pl1 = p[1][l]
 	pu0= p[0][u]
 	pu1 = p[1][u]
 
-	print "pl", pl
-	print "pu", pu
+	print "pl0", pl0
+	#print "pl1", pl1
+	print "pu0", pu0
+	#print "pu1", pu1
+
 	#iterate using dp array, within path bounds
 	print "we are comparing these strings:", A[mid: mid + m], B
 	for i in range(1,m +1):
 		index = mid + i
 		#print "char is", A[index]
 		for j in range(1,n+1):
-			print "index is", index, "upper is", pu[j], "lower is", pl[j]
-			if  (index >= pl[j] and index <= pu[j]) or noPath:
+			#print "index is", index, "upper is", pu[j], "lower is", pl[j]
+			#NOTE: BELOW WORKS FINE AND IS INDEED FASTER--GETS WEIRD WITH UPPER (original line
+			# is the one under it)--Liv
+			if  (index >= pl0[j]) or noPath:
+			#if  (index >= pl m[j] and index <= pu[j]) or noPath:
 				if A[index-1] == B[j-1]:
 					#print "!!!!! match when these two char:", A[index-1], B[j-1]
 					arr[index][j] = arr[index-1][j-1]+1
 				else:
 					#print "NO match when these two char:", A[index-1], B[j-1]
 					arr[index][j] = max(arr[index-1][j], arr[index][j-1])
-
-		#print arr
-		print
-		print
+	print "arr"
+	print arr
+	print
+	print
 	print "LCS of this sequence is", arr[m + mid][n]
 	LCS[mid] = arr[m + mid][n]
 	print "LCS is"
@@ -96,8 +142,8 @@ def FindShortestPath(A,B, l,u):
 	 mid = (l+u)/2
 	 print "mid is", mid, "l is", l, "u is", u
 
-	 SingleShortestPath(A_double,B,mid, l, u)
-	 #FindShortestPath(A_double,B,l,mid)
+	 SingleShortestPath(A_double,B,mid,l,u)
+	 FindShortestPath(A,B,l,mid)
 	 FindShortestPath(A,B,mid,u)
 
 
@@ -111,7 +157,7 @@ def CLCSFast(A,B):
 	n = len(B)
 
 	#initialize path array
-	path_l = path_u = np.zeros((m + 1,n+1), dtype=int)	
+	path_l = path_u = np.zeros((m + 1,n+1), dtype=int)
 	p = [path_l, path_u]
 
 	#initialize LCS array
