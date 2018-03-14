bc4568068305962a2c7212f110fa7d4895e7e3c2
Aditi Poduval <apoduval@stanford.edu>
2018-03-13 22:56:49 -0700

    fixed backtrace

--- CLCSFast.py
+++ CLCSFast.py
@@ -10,41 +10,47 @@ p = [[[]]]
 maxLCS = -1
 
 
+#important note, we would only need mid,l,u if we wanted to compare to path
 def PathBacktrace(A,B,mid,l,u):
-	i = len(A)/2
+	i = len(A)
 	j = len(B)
 
-	path = []
-	
+	path = [[0,0] for x in range(m + 1)]
+
+	path[i] =  [j,j]
+	print "path is", path
 	while i>=0 or j >=0: 
 		cur = arr[i][j]
-
+		#edge cases
+		#if we are in [0,0], we exit the while loop
 		if i == 0 and j == 0:
 			break
 
-		elif i == 0 and j != 0: # Go left 
-			path += arr[i - 1][j]
-			i -= 1
-
-		elif i = 0 and j != 0: # Go up 
-			path += arr[i][j-1]
+		#if we are in the first row, go left
+		elif i == 0 and j != 0: 
+			path[i][0] = [j-1]
 			j -= 1
+
+		#if we are in the first col, go up
+		elif i = 0 and j != 0: 
+			path[i-1] = [j][j]
+			i -= 1
 
 		elif i >= 1 and j >= 1:
 			diag = arr[i - 1][j - 1]
 			left = arr[i - 1][j]
 			top = arr[i][j-1]
 			if A[i] == B[j]:
-				path += diag
+				path[i-1] = [j-1][j-1]
 				i -= 1
 				j -= 1
 			# If not same, then find the larger of two and
 	        # go in the direction of larger value
 	        elif left >= top:
-	        	path += left
+	        	path[i][0] = [j-1]
 	            j-=1
 	        else:
-	        	path += top
+	        	path[i-1] = [j][j]
 	            i-=1	
 
 	return path
@@ -62,15 +68,6 @@ def SingleShortestPath(A,B,mid, l, u):
 	path = [0 for i in range(n)]
 
 	for i in range(1,m +1):
-
-
-		
-
-
-
-
-
-		
 		index = mid + i
 		#print "char is", A[index]
 		for j in range(1,n+1):
@@ -97,10 +94,6 @@ def SingleShortestPath(A,B,mid, l, u):
 
 	return
 
-
-
-
-
 # def FindShortestPath(A,B,p, l,u):
 def FindShortestPath(A,B, l,u):
 	 A_double = A + A
@@ -112,9 +105,25 @@ def FindShortestPath(A,B, l,u):
 	 FindShortestPath(A,B,l,mid)
 	 FindShortestPath(A,B,mid,u)
 
-def computeP0(A,B):
-	LCS(A,B)
+def LCS(A,B):
+	m = len(A)
+	n = len(B)
 
+	for i in range(1,m+1):
+		for j in range(1,n+1):
+			if A[i-1] == B[j-1]:
+				arr[i][j] = arr[i-1][j-1]+1
+			else:
+				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+
+	return PathBacktrace(A,B)
+
+def ComputeP0(A,B):
+	p[0] = LCS(A,B)
+
+def ComputePm(A,B):
+	m = len(A)
+	p[m] = LCS(A[m] + A[:m-1],B)
 
 def CLCSFast(A,B):
 	#define global variables
@@ -127,8 +136,9 @@ def CLCSFast(A,B):
 
 	#initialize path array
 	p = np.zeros(m,m, dtype=[]) #NOT SURE ABOUT DTYPE
-
-	FindShortestPath(A,B,0,m) 
+	ComputeP0(A,B)
+	#ComputePm(A,B)
+	#FindShortestPath(A,B,0,m) 
 	return maxLCS
 
 
