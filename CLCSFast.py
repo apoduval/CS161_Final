6aba0ab09c25cf967f5aa718d7b1a698410f9964
elanh <halpernesta@DN0a1f4862.SUNet>
2018-03-13 22:46:31 -0700

    Updates Single Shortest Path

--- CLCSFast.py
+++ CLCSFast.py
@@ -51,7 +51,7 @@ def PathBacktrace(A,B,mid,l,u):
 
 
 
-def SingleShortestPath(A,B,mid, l, u):
+def SingleShortestPath(A,B,mid,l,u):
 
 	global p
 	global arr
@@ -59,43 +59,32 @@ def SingleShortestPath(A,B,mid, l, u):
 	m = len(A)/2
 	n = len(B)
 
-	path = [0 for i in range(n)]
+	#path = [0 for i in range(n)]
+
 
 	for i in range(1,m +1):
-
-
-		
-
-
-
-
-
-		
+		plRight = p[l][i][1]
+		puLeft = p[u][i][0]
+		jrange = (0,n)
 		index = mid + i
-		#print "char is", A[index]
-		for j in range(1,n+1):
-			#print "index is", index, "upper is", pu[j], "lower is", pl[j]
-			#NOTE: BELOW WORKS FINE AND IS INDEED FASTER--GETS WEIRD WITH UPPER (original line
-			# is the one under it)--Liv
-			if  (index >= pl0[j]) or noPath:
-			#if  (index >= pl m[j] and index <= pu[j]) or noPath:
-				if A[index-1] == B[j-1]:
-					#print "!!!!! match when these two char:", A[index-1], B[j-1]
-					arr[index][j] = arr[index-1][j-1]+1
-				else:
-					#print "NO match when these two char:", A[index-1], B[j-1]
-					arr[index][j] = max(arr[index-1][j], arr[index][j-1])
-	#print "arr"
-	#print arr
-	# print
-	# print
-	print "LCS of this sequence is", arr[m + mid][n]
-	LCS[mid] = arr[m + mid][n]
-	#print "LCS is"
-	#print LCS
-	path = PathBacktrace(mid,m,l,u)
 
-	return
+		if i <= u and i >= l:
+			jrange = (0,plRight)
+		elif i > u and i > m:
+			jrange = (puLeft,n)
+		elif i > u:
+			jrange = (puLeft, plRight)
+
+		for j in jrange:
+			if A[index-1] == B[j-1]: # If we have a match 
+				arr[index][j] = arr[index-1][j-1]+1
+			else:
+				arr[index][j] = max(arr[index-1][j], arr[index][j-1])
+	print "LCS of this sequence is", arr[m + mid][n]
+
+	path = PathBacktrace(A,B,mid,l,u)
+
+return
 
 
 
